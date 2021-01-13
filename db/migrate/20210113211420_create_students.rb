776c0e60995cf95ada2f1a3f1319d4d3ea197dff
Jack Madden <jck13mad@gmail.com>
2021-01-13 21:15:31 +0000

    Automatically backed up by Learn

--- db/migrate/20210113211420_create_students.rb
+++ db/migrate/20210113211420_create_students.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-      t.timestamp 
+      t.timestamps null:flase
     end
   end
 end
